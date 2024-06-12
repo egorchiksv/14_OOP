925b2e55fe384fa1bc9a1723951d29d0baef4bac
Egor Saratov <npp_board@mail.ru>
2024-06-12 22:27:05 +0300

    Доделал

--- Seminar01/Task1/src/ProductMain.java
+++ Seminar01/Task1/src/ProductMain.java
@@ -5,9 +5,9 @@ public class ProductMain {
 
     public static void main(String[] args) {
 
-        Product bottle1 = new Product("Родники", 55, LocalDate.of(2024, 5, 1));
+        Product bottle1 = new BottleOfWater("Родники", 55, LocalDate.of(2024, 5, 1));
 
-        Product bottle2 = new Product("Родники Газированная", 55, LocalDate.of(2024, 5, 1),
+        Product bottle2 = new BottleOfWater("Родники Газированная", 55, LocalDate.of(2024, 5, 1),
                 true, EPackage.GLASS.getMaterial(), 0.5F);
 
         VendingMachine vm = new VendingMachine();
